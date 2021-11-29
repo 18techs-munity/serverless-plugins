3054b6b00af616acf4c1546a913901d7e1c91f25
Diego Barahona <diestrin@gmail.com>
2021-11-29 17:30:03 -0600

    fix: serverless-offline-s3 stuck initializing

--- packages/serverless-offline-s3/src/index.js
+++ packages/serverless-offline-s3/src/index.js
@@ -1,17 +1,17 @@
-const {get, isUndefined, omitBy, pick} = require('lodash/fp');
+const { get, isUndefined, omitBy, pick } = require("lodash/fp");
 
-const log = require('@serverless/utils/log').log;
+const log = require("@serverless/utils/log").log;
 
-const S3 = require('./s3');
+const S3 = require("./s3");
 
-const OFFLINE_OPTION = 'serverless-offline';
-const CUSTOM_OPTION = 'serverless-offline-s3';
+const OFFLINE_OPTION = "serverless-offline";
+const CUSTOM_OPTION = "serverless-offline-s3";
 
 const SERVER_SHUTDOWN_TIMEOUT = 5000;
 
 const defaultOptions = {
   batchSize: 100,
-  startingPosition: 'TRIM_HORIZON'
+  startingPosition: "TRIM_HORIZON",
 };
 
 const omitUndefined = omitBy(isUndefined);
@@ -28,10 +28,10 @@ class ServerlessOfflineS3 {
     this.serverless = serverless;
 
     this.hooks = {
-      'offline:start:init': this.start.bind(this),
-      'offline:start:ready': this.ready.bind(this),
-      'offline:start': this._startWithReady.bind(this),
-      'offline:start:end': this.end.bind(this)
+      "offline:start:init": this.start.bind(this),
+      "offline:start:ready": this.ready.bind(this),
+      "offline:start": this._startWithReady.bind(this),
+      "offline:start:end": this.end.bind(this),
     };
   }
 
@@ -40,7 +40,7 @@ class ServerlessOfflineS3 {
 
     this._mergeOptions();
 
-    const {s3Events, lambdas} = this._getEvents();
+    const { s3Events, lambdas } = this._getEvents();
 
     await this._createLambda(lambdas);
 
@@ -58,15 +58,15 @@ class ServerlessOfflineS3 {
   }
 
   ready() {
-    if (process.env.NODE_ENV !== 'test') {
+    if (process.env.NODE_ENV !== "test") {
       this._listenForTermination();
     }
   }
 
   _listenForTermination() {
-    const signals = ['SIGINT', 'SIGTERM'];
+    const signals = ["SIGINT", "SIGTERM"];
 
-    signals.map(signal =>
+    signals.map((signal) =>
       process.on(signal, async () => {
         this.serverless.cli.log(`Got ${signal} signal. Offline Halting...`);
 
@@ -81,11 +81,11 @@ class ServerlessOfflineS3 {
   }
 
   async end(skipExit) {
-    if (process.env.NODE_ENV === 'test' && skipExit === undefined) {
+    if (process.env.NODE_ENV === "test" && skipExit === undefined) {
       return;
     }
 
-    this.serverless.cli.log('Halting offline server');
+    this.serverless.cli.log("Halting offline server");
 
     const eventModules = [];
 
@@ -105,7 +105,7 @@ class ServerlessOfflineS3 {
   }
 
   async _createLambda(lambdas) {
-    const {default: Lambda} = await import('serverless-offline/lambda');
+    const { default: Lambda } = await import("serverless-offline/lambda");
     this.lambda = new Lambda(this.serverless, this.options);
 
     this.lambda.create(lambdas);
@@ -125,7 +125,7 @@ class ServerlessOfflineS3 {
 
   _mergeOptions() {
     const {
-      service: {custom = {}, provider}
+      service: { custom = {}, provider },
     } = this.serverless;
 
     const offlineOptions = custom[OFFLINE_OPTION];
@@ -135,37 +135,37 @@ class ServerlessOfflineS3 {
       {},
       omitUndefined(defaultOptions),
       omitUndefined(provider),
-      omitUndefined(pick(['location', 'localEnvironment'], offlineOptions)), // serverless-webpack support
+      omitUndefined(pick(["location", "localEnvironment"], offlineOptions)), // serverless-webpack support
       omitUndefined(customOptions),
       omitUndefined(this.cliOptions)
     );
 
-    log.debug('options:', this.options);
+    log.debug("options:", this.options);
   }
 
   _getEvents() {
-    const {service} = this.serverless;
+    const { service } = this.serverless;
 
     const lambdas = [];
     const s3Events = [];
 
     const functionKeys = service.getAllFunctions();
 
-    functionKeys.forEach(functionKey => {
+    functionKeys.forEach((functionKey) => {
       const functionDefinition = service.getFunction(functionKey);
 
-      lambdas.push({functionKey, functionDefinition});
+      lambdas.push({ functionKey, functionDefinition });
 
       const events = service.getAllEventsInFunction(functionKey) || [];
 
-      events.forEach(event => {
-        const {s3} = event;
+      events.forEach((event) => {
+        const { s3 } = event;
 
         if (s3 && functionDefinition.handler) {
           s3Events.push({
             functionKey,
             handler: functionDefinition.handler,
-            s3
+            s3,
           });
         }
       });
@@ -173,12 +173,15 @@ class ServerlessOfflineS3 {
 
     return {
       s3Events,
-      lambdas
+      lambdas,
     };
   }
 
   _getResources() {
-    const Resources = get(['service', 'resources', 'Resources'], this.serverless);
+    const Resources = get(
+      ["service", "resources", "Resources"],
+      this.serverless
+    );
     return Resources;
   }
 }
